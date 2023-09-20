b873d38fa66829735c35c882def820e7c3bec327
luminous <910667665@qq.com>
2023-09-21 05:42:50 +0800

    feat: 文本框组件添加

---
+++ src/components/QuestionComponents/QuestionParagraph/PropsComponent.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { Form, Input, Checkbox } from "antd";
+
+const { TextArea } = Input;
+
+const PropsComponent = (props) => {
+  const { text, isCenter, onChange, disabled } = props;
+
+  const [form] = Form.useForm();
+
+  // 监听组件的切换 当组件切换的时候，右侧组件的属性也跟着更新
+  useEffect(() => {
+    form.setFieldsValue({
+      text,
+      isCenter,
+    });
+  }, [text, isCenter, form]);
+
+  // 监听属性变化，同步到画布
+  function handleValueChange() {
+    if (onChange) {
+      onChange(form.getFieldsValue());
+    }
+  }
+
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      initialValues={{ text, isCenter }}
+      onValuesChange={handleValueChange}
+      disabled={disabled}
+    >
+      <Form.Item
+        label="段落内容"
+        name="text"
+        rules={[{ required: true, message: "请输入段落内容" }]}
+      >
+        <TextArea></TextArea>
+      </Form.Item>
+      <Form.Item name="isCenter" valuePropName="checked">
+        <Checkbox>居中显示</Checkbox>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default PropsComponent;
---
+++ src/components/QuestionComponents/QuestionParagraph/component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Typography } from "antd";
+
+const { Paragraph } = Typography;
+
+const QuestionParagraph = (props) => {
+  const { text = "一行段落", isCenter = false } = props;
+
+  // 换行实现: 方式一，但有安全危险
+  // const newText = text.replaceAll("\n", "<br>");
+
+  // 换行实现：方式二 => 根据换行符，切割成数组
+  const textList = text.split("\n");
+
+  return (
+    <Paragraph
+      style={{ textAlign: isCenter ? "center" : "start", marginBottom: 0 }}
+    >
+      {/* <span dangerouslySetInnerHTML={{ __html: newText }}></span> */}
+      {textList.map((t, index) => (
+        <span>
+          {/* 如果该文本不是第一个，则添加上一个br，换行 */}
+          {index > 0 && <br></br>}
+          {t}
+        </span>
+      ))}
+    </Paragraph>
+  );
+};
+
+export default QuestionParagraph;
